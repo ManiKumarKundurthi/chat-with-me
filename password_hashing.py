7da563682bf73f75ef374a41c3da702deeea3155
CodeWithMe <102638928+ManiKumarKundurthi@users.noreply.github.com>
2025-10-18 16:44:40 +0530

    Update password_hashing.py

--- password_hashing.py
+++ password_hashing.py
@@ -1,5 +1,5 @@
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
-password_hash = bcrypt.generate_password_hash('Dark@950').decode('utf-8')
+password_hash = bcrypt.generate_password_hash('YOUR_PASSWORD').decode('utf-8')
 print(password_hash)
